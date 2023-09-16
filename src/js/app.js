a083ab5634881790c2fd56be3dbf6f5a00ee1ab7
Work <wrapped@grade1s-MacBook-Pro.local>
2023-09-16 14:51:15 +0100

    Foundation elements in place for build

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,14 @@
+import "./slick";
+
 import $ from 'jquery';
 
 global.jQuery = global.$ = $;
 
 $(() => {
-    
+
+    $('.your-class').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
 });
